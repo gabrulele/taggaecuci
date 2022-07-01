2fac0a049eaa13e4d23f00a031305375e719857f
gabriele rizzitiello <frizzy2001@gmail.com>
2022-07-01 18:08:12 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/taggaecuci/TaggaecuciApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.taggaecuci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaggaecuciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaggaecuciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/taggaecuci/TaggaecuciApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.taggaecuci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaggaecuciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
